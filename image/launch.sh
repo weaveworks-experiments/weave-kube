b90bfd54fcbd12bad13198e4f5b6d4c5d5ed5b0d
Bryan Boreham <bjboreham@gmail.com>
2016-08-08 17:13:24 +0100

    Create CNI plugins by copying binaries rather than scripts using docker run

--- image/launch.sh
+++ image/launch.sh
@@ -2,7 +2,26 @@
 
 set -e
 
-/home/weave/weave --local setup-cni
+# Create CNI config, if not already there
+if [ ! -f /etc/cni/net.d/10-weave.conf ] ; then
+    mkdir -p /etc/cni/net.d
+    cat > /etc/cni/net.d/10-weave.conf <<EOF
+{
+    "name": "weave",
+    "type": "weave-net"
+}
+EOF
+fi
+
+# Copy CNI plugin binary
+if [ ! -f /opt/cni/bin/weave-net ] ; then
+    mkdir -p /opt/cni/bin
+    cp /home/weave/plugin /opt/cni/bin/weave-net
+fi
+if [ ! -f /opt/cni/bin/weave-ipam ] ; then
+    cp /home/weave/plugin /opt/cni/bin/weave-ipam
+fi
+
 /home/weave/weave --local create-bridge
 
 exec /home/weave/weaver --port=6783 --datapath=datapath \
