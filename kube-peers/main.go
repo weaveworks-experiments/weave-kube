c59e3489a1cb8316653f5514f79fe633efc431fc
Bryan Boreham <bjboreham@gmail.com>
2016-09-24 21:07:29 +0100

    Improve master detection again
    Call 'NewInCluster' first, then if we can't get through try the
    alternate approach using pkg/kubectl/cmd/util.

--- kube-peers/main.go
+++ kube-peers/main.go
@@ -6,27 +6,35 @@ import (
 	"os"
 
 	"k8s.io/kubernetes/pkg/api"
+	"k8s.io/kubernetes/pkg/client/restclient"
 	"k8s.io/kubernetes/pkg/client/unversioned"
 	kubectl_util "k8s.io/kubernetes/pkg/kubectl/cmd/util"
 )
 
 func getKubePeers() ([]string, error) {
-	factory := kubectl_util.NewFactory(nil)
-	config, err := factory.ClientConfig()
-	if err != nil {
-		os.Setenv("KUBERNETES_SERVICE_HOST", "")
-		os.Setenv("KUBERNETES_SERVICE_PORT", "")
-		config, err = factory.ClientConfig()
-		if err != nil {
-			log.Fatal("error contacting APIServer: ", err)
-		}
-	}
-
-	c, err := unversioned.New(config)
+	c, err := unversioned.NewInCluster()
 	if err != nil {
 		return nil, err
 	}
 	nodeList, err := c.Nodes().List(api.ListOptions{})
+	if err != nil {
+		// Fallback for cases (e.g. from kube-up.sh) where kube-proxy is not running on master
+		log.Print("error contacting APIServer: ", err, "; trying with blank env vars")
+		os.Setenv("KUBERNETES_SERVICE_HOST", "")
+		os.Setenv("KUBERNETES_SERVICE_PORT", "")
+		factory := kubectl_util.NewFactory(nil)
+		var config *restclient.Config
+		config, err = factory.ClientConfig()
+		if err != nil {
+			log.Fatal("error contacting APIServer: ", err)
+		}
+		c, err = unversioned.New(config)
+		if err != nil {
+			return nil, err
+		}
+		nodeList, err = c.Nodes().List(api.ListOptions{})
+	}
+
 	if err != nil {
 		return nil, err
 	}
