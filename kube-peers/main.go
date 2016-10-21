92dfd3f982f15214e2996903f58aacfa92b629b3
Bryan Boreham <bjboreham@gmail.com>
2016-10-21 14:52:18 +0100

    Change kube-peers to use client-go packages

--- kube-peers/main.go
+++ kube-peers/main.go
@@ -3,32 +3,27 @@ package main
 import (
 	"fmt"
 	"log"
-	"os"
 
-	"k8s.io/kubernetes/pkg/api"
-	"k8s.io/kubernetes/pkg/client/restclient"
-	"k8s.io/kubernetes/pkg/client/unversioned"
-	kubectl_util "k8s.io/kubernetes/pkg/kubectl/cmd/util"
+	"k8s.io/client-go/1.4/kubernetes"
+	"k8s.io/client-go/1.4/pkg/api"
+	"k8s.io/client-go/1.4/rest"
 )
 
 func getKubePeers() ([]string, error) {
-	c, err := unversioned.NewInCluster()
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		return nil, err
+	}
+	c, err := kubernetes.NewForConfig(config)
 	if err != nil {
 		return nil, err
 	}
 	nodeList, err := c.Nodes().List(api.ListOptions{})
 	if err != nil {
 		// Fallback for cases (e.g. from kube-up.sh) where kube-proxy is not running on master
-		log.Print("error contacting APIServer: ", err, "; trying with blank env vars")
-		os.Setenv("KUBERNETES_SERVICE_HOST", "")
-		os.Setenv("KUBERNETES_SERVICE_PORT", "")
-		factory := kubectl_util.NewFactory(nil)
-		var config *restclient.Config
-		config, err = factory.ClientConfig()
-		if err != nil {
-			log.Fatal("error contacting APIServer: ", err)
-		}
-		c, err = unversioned.New(config)
+		config.Host = "http://localhost:8080"
+		log.Print("error contacting APIServer: ", err, "; trying with fallback: ", config.Host)
+		c, err = kubernetes.NewForConfig(config)
 		if err != nil {
 			return nil, err
 		}
